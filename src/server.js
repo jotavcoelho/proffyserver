5df3f377f368eb894ce64e1b2f0c8f4f03fcefa4
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-29 12:25:11 -0300

    Sqlite has been set up, with knex, and first migration made

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response) => {
   console.log("Hullow");
   return response.json({ "hello": "world"});
