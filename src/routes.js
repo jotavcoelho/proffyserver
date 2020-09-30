485c5240e4fb2e51162cb373dadf3fdc54b16f43
J. Vitor Coelho <jvcoelho.mail@gmail.com>
2020-09-29 21:35:43 -0300

    Added route to create user and classes

---
+++ src/routes.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import db from './database/connection';
+
+import convertHoursToMinutes from './utils/convertHoursToMinutes';
+
+const routes = express.Router();
+
+routes.post('/classes', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body;
+
+  try {
+    const trx = await db.transaction();
+
+    const insertedUserIds = await trx('users').insert({
+      name,
+      avatar,
+      whatsapp,
+      bio
+    });
+
+    const user_id = insertedUserIds[0];
+
+    const insertedClassIds = await trx('classes').insert({
+      subject,
+      cost,
+      user_id,
+    });
+
+    const class_id = insertedClassIds[0];
+
+    const classSchedule = schedule.map(scheduleItem => {
+      return {
+        class_id,
+        weekday: scheduleItem.weekday,
+        from: convertHoursToMinutes(scheduleItem.from),
+        to: convertHoursToMinutes(scheduleItem.to),
+      };
+    });
+
+    await trx('class_schedule').insert(classSchedule);
+
+    await trx.commit();
+
+    return response.status(201).send();
+  } catch (err) {
+    await trx.rollback();
+
+    return response.status(400).json({
+      error: err,
+    });
+  }
+
+});
+
+export default routes;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  console.log("Hullow");
-  return response.json({ "hello": "world"});
-});
+app.use(routes);
 
 app.listen(3333);
